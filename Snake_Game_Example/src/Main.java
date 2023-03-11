31549b1278fdd752bbc796a6a278b5b2f615f4fc
Roman Manzhula <roman1985al@ukr.net>
2023-03-11 21:28:28 +0200

    Add files via upload

---
+++ Snake_Game_Example/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        WindowGame windowGame = new WindowGame(); //создаем новый обьект типа и поведения класса ОкноИгры
+    }
+}
